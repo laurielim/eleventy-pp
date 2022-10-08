715b6aa0b4fd9b318deb3e4d5566ad6930240513
laurielim <laurie.limsam@gmail.com>
2022-10-08 12:30:19 +0300

    Don't output index files for products

--- src/_11ty/transforms/minify.js
+++ src/_11ty/transforms/minify.js
@@ -4,13 +4,15 @@ const htmlmin = require("./htmlmin");
 const jsmin = require("./jsmin");
 
 module.exports = async (content, outputPath) => {
-  const ext = extname(outputPath);
-  switch (ext) {
-    case ".html":
-      return htmlmin(content, outputPath);
-    case ".js":
-      return jsmin(content, outputPath);
-    default:
-      return content;
+  if (outputPath) {
+    const ext = extname(outputPath);
+    switch (ext) {
+      case ".html":
+        return htmlmin(content, outputPath);
+      case ".js":
+        return jsmin(content, outputPath);
+      default:
+        return content;
+    }
   }
 }
