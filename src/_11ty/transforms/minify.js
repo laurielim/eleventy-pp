68588ea8663a33212cc0228b125cff1582323bc6
laurielim <laurie.limsam@gmail.com>
2022-10-20 10:56:10 +0300

    Fix warning issue

--- src/_11ty/transforms/minify.js
+++ src/_11ty/transforms/minify.js
@@ -4,15 +4,15 @@ const htmlmin = require("./htmlmin");
 const jsmin = require("./jsmin");
 
 module.exports = async (content, outputPath) => {
-  if (outputPath) {
-    const ext = extname(outputPath);
-    switch (ext) {
-      case ".html":
-        return htmlmin(content, outputPath);
-      case ".js":
-        return jsmin(content, outputPath);
-      default:
-        return content;
-    }
+  if (!outputPath) return content;
+
+  const ext = extname(outputPath);
+  switch (ext) {
+    case ".html":
+      return htmlmin(content, outputPath);
+    case ".js":
+      return jsmin(content, outputPath);
+    default:
+      return content;
   }
 }
