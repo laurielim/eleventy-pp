3b84afc23b333f27bc5fbb82ac27331dbc4ca658
laurielim <laurie.limsam@gmail.com>
2022-11-25 13:04:34 +0200

    Add meta desc to plastic categories

--- src/content/en/plastics/plastics.11tydata.js
+++ src/content/en/plastics/plastics.11tydata.js
@@ -3,6 +3,7 @@ module.exports = {
   permalink: "/{{ locale }}/plastics/{{ title | slugify }}/index.html",
   tags: ['plasticCategories_en'],
   eleventyComputed: {
+    metaDesc: data => `Browse the list of ${data.title.toLowerCase()} we have in stock. Available immediate on premises.`,
     eleventyNavigation: {
       key: data => data.page.fileSlug,
       title: data => data.title,
--- src/content/fr/plastics/bags-on-roll.md
+++ src/content/fr/plastics/bags-on-roll.md
@@ -1,6 +1,5 @@
 ---
 title: Sacs en rouleaux
-description: ""
 image: src/assets/images/bags-on-roll.jpeg
 imageAlt: Un sac en rouleau
 products:
