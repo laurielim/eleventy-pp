b25e851de507331aeb51f7c511b3890432d71570
laurielim <laurie.limsam@gmail.com>
2022-12-09 11:40:53 +0200

    Add equipment CTA

--- src/equipment/productCategory/fr/fr.11tydata.js
+++ src/equipment/productCategory/fr/fr.11tydata.js
@@ -1,5 +1,11 @@
 module.exports = {
   locale: "fr",
+  equipmentCTA: {
+    title: "Développons votre entreprise",
+    summary: "Travaillons ensamble pour améliorer votre rapidité et votre efficacité de production. Nous accueillons aussi bien les PME que les grandes entreprises.",
+    buttonText: "Passer une commande",
+    buttonUrl: "tel:+230 5931 6762",
+  },
   eleventyComputed: {
     eleventyNavigation: {
       key: data => data.page.fileSlug,
